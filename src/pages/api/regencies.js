ee74d67ef94056c9646f386b3c3e2835cfb58266
Fajar Nur Prasetyo <fajarnurprasetyo.2@gmail.com>
2023-08-09 01:55:45 +0700

    update regencies.js

--- src/pages/api/regencies.js
+++ src/pages/api/regencies.js
@@ -6,21 +6,30 @@ import prisma from "../../prisma";
  */
 export default async ({ query }, res) => {
   const provinceId = parseInt(query.provinceId);
-  // const search = query.search;
-  // const skip = parseInt(query.skip);
-  // const take = parseInt(query.take);
+  const search = query.search;
+  const skip = parseInt(query.skip);
+  const take = parseInt(query.take);
 
   const check1 = !isNaN(provinceId);
-  // const check2 = typeof search === 'string' && search.length > 0;
-  // const check3 = !isNaN(skip) && !isNaN(take);
+  const check2 = typeof search === 'string' && search.length > 0;
 
-  if (!check1) {
+  if (check1 === check2) {
     res.status(400).end();
     return;
   }
 
+  if (check1) {
+    const regencies = await prisma.regency.findMany({
+      where: { provinceId }
+    });
+    res.json(regencies);
+    return;
+  }
+
   const regencies = await prisma.regency.findMany({
-    where: { provinceId }
+    where: { name: { contains: search } },
+    skip: !isNaN(skip) ? skip : 0,
+    take: !isNaN(take) ? take : 20,
   });
   res.json(regencies);
 }
